c8913a8013f0037513b93fa3c87c3e07643f1dc6
richbruton <richbruton@gmail.com>
2022-02-13 16:59:09 +0000

    add script for player selection and computer random choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,48 @@
 // wait for page to load
 
+let choices = ["paper", "rock", "scissors"]
+
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
+
+    // player choice and computer choice
     for (let button of buttons) {
         button.addEventListener("click", function () {
+
             if (this.getAttribute("data-type") === "rock") {
-                alert("You picked rock");
+                let p1Choice = "rock";
+                let aiChoice = choices[Math.floor(Math.random() * choices.length)];
+                console.log(aiChoice);
             } else if (this.getAttribute("data-type") === "paper") {
-                alert("You picked paper");
+                let p1Choice = "paper";
+                let aiChoice = choices[Math.floor(Math.random() * choices.length)];
             } else if (this.getAttribute("data-type") === "scissors") {
-                alert("You picked scissors");
+                let p1Choice = "scissors";
+                let aiChoice = choices[Math.floor(Math.random() * choices.length)];
             }
         })
     }
 })
+
+
+function checkDrawGame() {
+    if (p1Choice === aiChoice) {
+        updateDrawScore();
+        console.log("draw")
+    } else {
+        gameWinner();
+    }
+}
+
+// function gameWinner()
+
+// function updateDrawScore()
+
+// function updateP1Score()
+
+// function UpdateAiScore()
+
+// function contestWinner()
+
+// function newGame()
